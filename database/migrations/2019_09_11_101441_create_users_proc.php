a73541bc86e6a55927c5991dd0e71b9f302c50ca
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2019-10-01 14:07:18 +0700

    user proc

--- database/migrations/2019_09_11_101441_create_users_proc.php
+++ database/migrations/2019_09_11_101441_create_users_proc.php
@@ -18,6 +18,8 @@ class CreateUsersProc extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('bank_account');
             $table->unsignedBigInteger('bank_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('origin_id');
             $table->decimal('saldo', 18, 2)->default(0);
 
             $table->unsignedBigInteger('created_by');
@@ -25,6 +27,8 @@ class CreateUsersProc extends Migration
 
             $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
             $table->foreign('bank_id')->on('master_bank')->references('id')->onDelete('cascade');
+            $table->foreign('category_id')->on('master_category')->references('id')->onDelete('cascade');
+            $table->foreign('origin_id')->on('master_origin')->references('id')->onDelete('cascade');
             $table->foreign('created_by')->on('users')->references('id')->onDelete('cascade');
             $table->foreign('updated_by')->on('users')->references('id')->onDelete('cascade');
 
