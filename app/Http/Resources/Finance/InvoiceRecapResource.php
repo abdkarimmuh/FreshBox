889d33317fb2597636e6bf1400ab1f6c12eefcf5
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-10-15 18:59:15 +0700

    Update API Invoice Recap

--- app/Http/Resources/Finance/InvoiceRecapResource.php
+++ app/Http/Resources/Finance/InvoiceRecapResource.php
@@ -9,11 +9,19 @@ class InvoiceRecapResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'customer_name' => $this->customer->name,
+            'up' => $this->customer->pic_customer,
+            'recap_invoice_no' => $this->recap_invoice_no,
+            'recap_date' => $this->recap_date,
+            'is_paid' => $this->is_paid,
+            'invoice_recap_detail' => InvoiceRecapDetailResource::collection($this->invoice_recap_detail)
+        ];
     }
 }
--- app/Model/Finance/InvoiceRecap.php
+++ app/Model/Finance/InvoiceRecap.php
@@ -10,6 +10,11 @@ class InvoiceRecap extends Model
     protected $table = 'trx_invoice_recap';
     protected $fillable = ['customer_id', 'recap_invoice_no', 'recap_date', 'created_by'];
 
+    public function invoice_recap_detail()
+    {
+        return $this->hasMany(InvoiceRecapDetail::class, 'invoice_recap_id', 'id');
+    }
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
