0bc1788194260d79d45a305607f8db6f611b3e7b
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-10-15 19:17:43 +0700

    Update Invoice Recap Detail API

--- app/Http/Resources/Finance/InvoiceRecapDetailResource.php
+++ app/Http/Resources/Finance/InvoiceRecapDetailResource.php
@@ -9,11 +9,16 @@ class InvoiceRecapDetailResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'invoice_no' => $this->invoice->invoice_no,
+            'send_date' => $this->invoice->delivery_order->do_date->formatLocalized('%d-%b-%y'),
+            'price' => $this->invoice->total_price
+        ];
     }
 }
--- app/Model/Finance/InvoiceRecapDetail.php
+++ app/Model/Finance/InvoiceRecapDetail.php
@@ -8,7 +8,12 @@ class InvoiceRecapDetail extends Model
 {
     protected $table = 'trx_invoice_recap_detail';
 
-    protected $fillable = ['invoice_recap_id','invoice_id'];
+    protected $fillable = ['invoice_recap_id', 'invoice_id'];
+
+    public function invoice()
+    {
+        return $this->belongsTo(InvoiceOrder::class, 'invoice_id');
+    }
 
     public function recap_invoice()
     {
