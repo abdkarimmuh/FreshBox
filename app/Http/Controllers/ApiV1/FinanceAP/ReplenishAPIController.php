ba884bd766250da862507fea034dfd138954f67c
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-11-19 10:39:31 +0700

    Add Finance Replenish

---
+++ app/Http/Controllers/ApiV1/FinanceAP/ReplenishAPIController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApiV1\FinanceAP;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReplenishAPIController extends Controller
+{
+    //
+}
