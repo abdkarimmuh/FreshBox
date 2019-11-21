e800b14604eb0479cde996a513a340b776dfe8b2
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2019-11-21 15:02:51 +0700

    replenihs

--- database/migrations/2019_11_21_071320_create_trx_topup_proc.php
+++ database/migrations/2019_11_21_071320_create_trx_topup_proc.php
@@ -8,21 +8,22 @@ class CreateTrxTopupProc extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('trx_topup_proc', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_proc_id');
+            $table->integer('amount')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->string('remark')->nullable();
+            $table->unsignedBigInteger('created_by');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
