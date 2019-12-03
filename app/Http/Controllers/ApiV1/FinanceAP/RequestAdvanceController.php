2b7fba80f12739232bc72fe55fb73ddb75257f70
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-12-03 14:01:41 +0700

    Add RequestAdvanceController

---
+++ app/Http/Controllers/ApiV1/FinanceAP/RequestAdvanceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\ApiV1\FinanceAP;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RequestAdvanceController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+
+}
