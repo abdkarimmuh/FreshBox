b5d4de1ea9897c17e4c479f1074ce035ef8971be
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-12-04 13:28:16 +0700

    Add Migration master_warehouse & user_profile

---
+++ database/migrations/2019_12_04_042708_create_user_profile_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('dept');
+            $table->string('no_rek');
+            $table->string('nama_rek');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
