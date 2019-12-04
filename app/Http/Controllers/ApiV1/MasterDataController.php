107b4b12a522e247720791fb0ac5a240fca2bd56
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-12-04 14:00:53 +0700

    Update Php Doc

--- app/Http/Controllers/ApiV1/MasterDataController.php
+++ app/Http/Controllers/ApiV1/MasterDataController.php
@@ -11,21 +11,38 @@ use Illuminate\Http\Request;
 
 class MasterDataController extends Controller
 {
+    /**
+     * Display a listing of the bank.
+     *
+     * @return Bank[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function getBank()
     {
         return Bank::all();
     }
 
+    /**
+     * Display a listing of the Origin.
+     * @return Origin[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function getOrigin()
     {
         return Origin::all();
     }
 
+    /**
+     * Display a listing of the Category.
+     * @return Category[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function getCategory()
     {
         return Category::all();
     }
 
+    /**
+     * Display a listing of the Customer Group.
+     * @return mixed
+     */
     public function getCustomerGroup()
     {
         return CustomerGroup::select('id', 'name')->get();
