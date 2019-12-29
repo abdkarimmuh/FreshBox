536881b113b52ce153a1903f35db9711e4441bd8
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-12-29 18:12:26 +0700

    update

--- resources/js/components/FinanceAP/PettyCash/IndexPettyCash.vue
+++ resources/js/components/FinanceAP/PettyCash/IndexPettyCash.vue
@@ -1,13 +1,70 @@
 <template>
-
+    <div v-if="$parent.userRole('Admin')">
+        <s-table :config="config" :columns="columns"/>
+    </div>
+    <div v-else>
+        <s-error-page :error="error"/>
+    </div>
 </template>
-
 <script>
     export default {
-        name: "IndexPettyCash"
-    }
+        data() {
+            return {
+                config: {
+                    title: "Request Finance",
+                    action: true,
+                    base_url: this.$parent.MakeUrl("api/v1/finance-ap/request-finance"),
+                    route_view: "finance.requestFinance.show",
+                    route_create: "finance.requestFinance.create"
+                    // route_multiple_print: 'invoice_order.multiplePrint',
+                },
+                columns: [
+                    {
+                        title: "No Request",
+                        field: "no_request",
+                        filterable: true
+                    },
+                    {
+                        title: "Request Date",
+                        field: "request_date",
+                        filterable: true
+                    },
+                    {
+                        title: "User Name",
+                        field: "user_name",
+                        filterable: false
+                    },
+                    {
+                        title: "Shipping Address",
+                        field: "shipping_address",
+                        filterable: true
+                    },
+                    {
+                        title: "Dept",
+                        field: "dept",
+                        filterable: true
+                    },
+                    {
+                        title: 'Status',
+                        field: 'status_name',
+                        type: 'html',
+                    },
+                    {
+                        title: "Created At",
+                        field: "created_at",
+                        filterable: true
+                    },
+                    {
+                        title: "Created By",
+                        field: "created_by_name",
+                        filterable: true
+                    }
+                ],
+                error: {
+                    code: 403,
+                    description: "You do not have access to this page"
+                }
+            };
+        }
+    };
 </script>
-
-<style scoped>
-
-</style>
